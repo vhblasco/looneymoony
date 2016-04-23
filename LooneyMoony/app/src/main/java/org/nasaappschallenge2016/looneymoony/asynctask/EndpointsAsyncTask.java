f037ef2ab377e0f7aff5c2f4ebfd9bd05b9bf8eb
vhblasco <karel81@gmail.com>
2016-04-23 23:03:45 +0100

    Adding new comments

--- LooneyMoony/app/src/main/java/org/nasaappschallenge2016/looneymoony/asynctask/EndpointsAsyncTask.java
+++ LooneyMoony/app/src/main/java/org/nasaappschallenge2016/looneymoony/asynctask/EndpointsAsyncTask.java
@@ -10,21 +10,22 @@ import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
-import org.nasaappschallenge2016.looneymoony.backend.myApi.MyApi;
+import org.nasaappschallenge2016.looneymoony.backend.lmApi.LmApi;
+import org.nasaappschallenge2016.looneymoony.backend.lmApi.model.FunFact;
 
 import java.io.IOException;
 
 /**
- * Created by oli on 23/04/16.
+ * Created by vhblasco on 23/04/16.
  */
 public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
-    private static MyApi myApiService = null;
+    private static LmApi myApiService = null;
     private Context context;
 
     @Override
     protected String doInBackground(Pair<Context, String>... params) {
         if(myApiService == null) {  // Only do this once
-            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+            LmApi.Builder builder = new LmApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
@@ -45,7 +46,10 @@ public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, S
         String name = params[0].second;
 
         try {
-            return myApiService.sayHi(name).execute().getData();
+
+            /* Concept test. The idea is return the Object */
+            FunFact funFact = myApiService.getFanFact().execute();
+            return funFact.getTitle();
         } catch (IOException e) {
             return e.getMessage();
         }
@@ -53,6 +57,8 @@ public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, S
 
     @Override
     protected void onPostExecute(String result) {
+
+        /* Concept Test. The idea is show the information in the activity */
         Toast.makeText(context, result, Toast.LENGTH_LONG).show();
     }
 }
