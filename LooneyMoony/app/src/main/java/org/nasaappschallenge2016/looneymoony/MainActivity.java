8e0c875898cf587a650a84338da290d8ee6289f1
vhblasco <karel81@gmail.com>
2016-04-23 15:13:31 +0100

    App linked with Google App Engine Java Endpoints (concept test)

--- LooneyMoony/app/src/main/java/org/nasaappschallenge2016/looneymoony/MainActivity.java
+++ LooneyMoony/app/src/main/java/org/nasaappschallenge2016/looneymoony/MainActivity.java
@@ -1,13 +1,17 @@
 package org.nasaappschallenge2016.looneymoony;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Pair;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.nasaappschallenge2016.looneymoony.asynctask.EndpointsAsyncTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+
     }
 
 
